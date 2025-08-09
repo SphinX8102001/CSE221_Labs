f8ba6e899edc2866fb9d38b5bfa72cfec421571d
SphinX8102001 <alwashisarker@gmail.com>
2025-08-09 22:56:43 +0600

    From Dipto (corrected and accepted)

--- Lab4/B.py
+++ Lab4/B.py
@@ -1,15 +1,17 @@
-def AdjListTOMatrix():
+#B
+
+def matrix():
     N, M = map(int, input().split())
     u_list = list(map(int, input().split()))
+    v_list = list(map(int, input().split()))
     w_list = list(map(int, input().split()))
-    w_l = list(map(int, input().split()))
 
     new_list = [[] for _ in range(N)]
 
     for i in range(M):
         u = u_list[i]
-        v = w_list[i]
-        w = w_l[i]
+        v = v_list[i]
+        w = w_list[i]
         new_list[u - 1].append((v, w))
 
     for k in range(N):
@@ -18,4 +20,4 @@ def AdjListTOMatrix():
             print(f"({neighbor},{weight})", end=" ")
         print()
 
-AdjListTOMatrix()
+matrix()
